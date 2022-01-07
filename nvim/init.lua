d36eaa628ea7405eafdf600e9fba1e8ad38186f5
Carlos Estrada <kachiiz@gmail.com>
2022-01-07 03:04:21 -0600

    Adding CMP to plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,5 @@ require "kachyz.options"
 require "kachyz.keymaps"
 require "kachyz.plugins"
 require "kachyz.colorscheme"
+require "kachyz.cmp"
+
--- nvim/lua/kachyz/plugins.lua
+++ nvim/lua/kachyz/plugins.lua
@@ -50,6 +50,18 @@ return packer.startup(function(use)
   use 'folke/tokyonight.nvim'
   use "Julpikar/night-owl.nvim"  -- night-owl theme
 
+  -- CMP plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
