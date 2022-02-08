d29253fd67491df04e930941c034fbba4ba39001
Carlos Estrada <kachiiz@gmail.com>
2022-02-08 12:35:05 -0600

    Adding missing filetypes for treesitter

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
-    "html", "javascript", "lua"
+    "html", "javascript", "lua", "typescript", "tsx", "css", "json"
   }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,
