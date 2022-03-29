36f617708c66bf782851c1fab08aa0f424745369
Carlos Estrada <kachiiz@gmail.com>
2022-03-29 10:13:27 -0600

    Update packages according to latest versions

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -21,7 +21,7 @@ telescope.setup {
         sorting_strategy = "ascending",
         layout_strategy = "horizontal",
         file_sorter = require'telescope.sorters'.get_fuzzy_file,
-        file_ignore_patterns = {},
+        file_ignore_patterns = {"package-lock.json"},
         generic_sorter =require'telescope.sorters'.get_generic_fuzzy_sorter,
         path_display = {},
         winblend = 0,
--- nvim/lua/user/themes/nightfox.lua
+++ nvim/lua/user/themes/nightfox.lua
@@ -3,17 +3,19 @@ local nightfox = require('nightfox')
 -- This function set the configuration of nightfox. If a value is not passed in the setup function
 -- it will be taken from the default configuration above
 nightfox.setup({
-  fox = "nightfox", -- duskfox is another good option
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "italic,bold" -- styles can be a comma separated list
+  -- fox = "nightfox", -- duskfox is another good option
+  options = {
+    styles = {
+      comments = "italic", -- change style of comments to be italic
+      keywords = "bold", -- change style of keywords to be bold
+      functions = "italic,bold" -- styles can be a comma separated list
+    },
+    inverse = {
+      match_paren = true, -- inverse the highlighting of match_parens
+    },
   },
-  inverse = {
-    match_paren = true, -- inverse the highlighting of match_parens
-  },
-  colors = { },
-  hlgroups = {
+  pallets = { },
+  groups = {
     GitSignsAdd = { fg = "#8EDF39" },
     GitSignsChange = { fg = "#F836B3" },
     GitSignsDelete = { fg = "#D80B06" },
@@ -22,4 +24,4 @@ nightfox.setup({
 })
 
 -- Load the configuration set above and apply the colorscheme
-nightfox.load()
+vim.cmd("colorscheme nightfox")
