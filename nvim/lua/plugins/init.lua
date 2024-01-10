afe5c11465c0ee6708e24a4df7d367826e3e3eec
Carlos Estrada <kachiiz@gmail.com>
2024-01-10 13:32:28 -0600

    update luasnip import

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -31,7 +31,10 @@ return {
       { "hrsh7th/cmp-nvim-lua" },
 
       -- Snippets
-      { "L3MON4D3/LuaSnip" },
+      { "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+      },
       { "rafamadriz/friendly-snippets" },
     },
   },
