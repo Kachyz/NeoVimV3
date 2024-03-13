8bcb7e29f606c248fc9d6534cb88661e4eeaaae0
Carlos Estrada <carlos.estrada@nextiva.com>
2024-03-13 11:31:41 -0600

    Update lualine fields

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -110,11 +110,13 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    --[[ lualine_b = {'branch', 'diff', 'diagnostics'}, ]]
+    lualine_b = {'branch'},
     lualine_c = {'filename'},
     lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = {'diff', 'diagnostics'},
+    --[[ lualine_z = {'location'} ]]
+    lualine_z = {}
   },
   inactive_sections = {
     lualine_a = {},
